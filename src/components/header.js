6c77afb1a9633b1ecdc4474f272b6a1a78b0f01d
Akansha200 <70310108+Akansha200@users.noreply.github.com>
2021-04-28 15:46:49 +0530

    Add files via upload

---
+++ src/components/header.js
@@ -0,0 +1,21 @@
+import React from 'react' ;
+const Header = () =>{
+    return(
+        <div>
+            <nav>
+                <ul>
+                    <li>
+                        <a href="#">Home</a>
+                    </li>
+                    <li>
+                        <a href="#">Cart</a>
+                    </li>
+                    <li>
+                        <a href="#">Register</a>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+    )
+}
+export default Header;
---
+++ src/components/main.js
@@ -0,0 +1,17 @@
+import React from 'react';
+const SiteInfo = () => {
+    return (
+        <div className="site-info">
+            <div className="title">
+                Syntup
+            </div>
+            <div className="xyz">
+                 Site having amazing products..
+            </div>
+            <div classNmae="itle">
+                shop here| explore here
+            </div>
+        </div>
+    )
+}
+export default SiteInfo;
